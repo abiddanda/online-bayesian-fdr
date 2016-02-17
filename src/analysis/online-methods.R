0b17bcc54f1be4225c7cb73066ffe64c58527a05
Arjun Biddanda <aab227@cornell.edu>
2016-02-16 23:34:30 -0600

    estimators for pFDR and FDR from data and sequentially-need to adaptively set gamma according to them

--- src/analysis/online-methods.R
+++ src/analysis/online-methods.R
@@ -21,10 +21,51 @@ estimate.nulls <- function(p, lambda = 0.5){
 # 			Storey 2002
 # 	tau = min. starting point
 # --------------------------
-estimate.nulls.sequential <- function(p, lambda=0.5, tau=100){
-  if (length(p) < tau){stop()}
+estimate.nulls.seq <- function(p, lambda=0.5, tau=100){
+  if (length(p) < tau){stop("Not enough p-values!")}
   n <- length(p)
   pi0.t <- vapply(seq(tau,n), FUN = function(x){estimate.nulls(p[1:x], lambda=lambda)}, FUN.VALUE = 1)
   return(pi0.t)
 }
 
+
+# --------------------------
+# Estimate pFDR (from Storey)
+# Args:
+#   p = vector of sequential 
+#     p-values
+#   pi0 = sequential prop. 
+#     nullss
+# --------------------------
+estimate.pfdr <- function(p, pi0, gamma){
+  m <- length(p)
+  R <- sum(p < gamma)
+  pr.gamma <- ifelse(R == 0, 1, R) / m
+  return((pi0*gamma)/ (pr.gamma * (1 - (1-gamma)^m)))
+}
+
+estimate.pfdr.seq <- function(p, pi0.t, gamma, tau=100){
+  n <- length(p)
+  pfdr.t <- vapply(seq(tau,n), FUN = function(x){estimate.pfdr(p[1:x], pi0.t[x-tau+1], gamma)}, FUN.VALUE = 1)
+  return(pfdr.t)
+}
+
+# --------------------------
+# Estimate FDR (from Storey)
+# Args:
+#   p = vector of p-values
+#   pi0 = current estimate 
+#     of pi0
+# --------------------------
+estimate.fdr <- function(p, pi0, gamma){
+  m <- length(p)
+  R <- sum(p < gamma)
+  pr.gamma <- ifelse(R==0, 1, R) / m
+  return((pi0*gamma)/(pr.gamma))
+}
+
+estimate.fdr.seq <- function(p, pi0.t, gamma, tau=100){
+  n <- length(p)
+  pfdr.t <- vapply(seq(tau,n), FUN = function(x){estimate.fdr(p[1:x], pi0.t[x-tau+1], gamma)}, FUN.VALUE = 1)
+  return(pfdr.t)
+}
