2c41b638273302cc17668ddcae47f11425d39603
Arjun Biddanda <aab227@cornell.edu>
2016-02-18 14:40:37 -0600

    changed some typos and notation-and added sim plot

--- src/simulation/simulate_mixture.R
+++ src/simulation/simulate_mixture.R
@@ -6,33 +6,17 @@
 # Args:
 #		pi0 - proportion from component1
 #		mu0 - mean of nulls
-#   sd1 - sd of nulls
+#   sd1 - var of nulls
 #   mu1 - mean of signals
-#   sd1 - sd of signals
+#   sd1 - var of signals
 #-----------------------------------
-sim.mixture.2comp <- function(n, pi0, mu0, sd0, mu1, sd1){
+sim.mixture.2comp <- function(n, pi0, mu0=0, sigma0=1, mu1, sigma1){
 	signals <- rbinom(n, 1, (1-pi0))
-	x <- vapply(signals,
-	            FUN = function(x){ifelse(x,rnorm(1,mu1,sd1), rnorm(1,mu0,sd0))}, 
+	z <- vapply(signals,
+	            FUN = function(x){ifelse(x,rnorm(1,mu1,sigma1), rnorm(1,mu0,sigma0))}, 
 	            FUN.VALUE = 1)
-	# TODO : simulate p-values from the mixture?
-	pvals <- 2*pnorm(-abs((x-mu0)/sd0))
-	mixture.df <- data.frame(true_signals = signals, X=x, P=pvals)
+	pvals <- 2*pnorm(-abs((z-mu0)/sqrt(sigma0)))
+	mixture.df <- data.frame(true_signals = signals, Z=z, P=pvals)
 	return(mixture.df)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
