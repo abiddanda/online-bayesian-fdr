23d2ffdb6b9a30a59fcd9f4e0d5647d3bcc06596
Arjun Biddanda <aab227@cornell.edu>
2016-03-14 09:12:45 -0500

    added data and plots for joe to touch up

--- src/analysis/fpr_power.R
+++ src/analysis/fpr_power.R
@@ -4,39 +4,84 @@
 # ------------------------------------
 
 # Sourcing Relevant Methods
-source("fdr_methods.R")
-source("lbond_lbord_ainvesting.R")
+source("~/Repos/online-bayesian-fdr/src/analysis/fdr_methods.R")
+source("~/Repos/online-bayesian-fdr/src/analysis/lbond_lbord_ainvesting.R")
+source("~/Repos/online-bayesian-fdr/src/model/mcmc.R")
 
+# Reading in Data
+sim10000 <- readRDS("~/Repos/online-bayesian-fdr/data/sim1.rds")
+sim500 <- readRDS("~/Repos/online-bayesian-fdr/data/sim2_500_08_3_1.rds")
 
-# Sourcing Data
-X <- readRDS("~/Repos/online-bayesian-fdr/data/sim1.rds")
-
-# Converting Z-scores to Pvals
-convert_Z_to_Pval <- function(mixture.sim, alpha=0.1){
-  Zs <- mixture.sim$Z
-  return(pnorm(-abs(Zs)))
+# Method for Sequential Gibbs Updates
+sequential_gibbs <- function(data, timestep, n_iter=1000, n_burnin=50, sampling_interval=10){
+  n <- length(data)
+  if (n %% timestep != 0){stop("Not an even divisor!")}
+  
+  ncolumns <- (n_iter - n_burnin) / sampling_interval
+  print(ncolumns)
+  mu.mat <- matrix(NA, nrow=(n/timestep), ncol = ncolumns)
+  pi0.mat <- matrix(NA, nrow=(n/timestep), ncol= ncolumns)
+  sigma2.mat <- matrix(NA, nrow=(n/timestep), ncol=ncolumns)
+  mat.ind <- 1
+  for (t in seq(1, n, timestep)){
+    print(t)
+    data.t <- data[1:t]
+    cur.param.samples <- gibbs_sampler(data.t, n_iter, sampling_interval = 10)
+    mu.mat[mat.ind, ] <- cur.param.samples$mu1_samples
+    pi0.mat[mat.ind, ] <- cur.param.samples$pi0_samples
+    sigma2.mat[mat.ind, ] <- 1/ cur.param.samples$phi1_samples
+    mat.ind <- mat.ind + 1  
+  }
+  return(list(mu.samples=mu.mat, pi0.samples=pi0.mat, sigma2.samples=sigma2.mat))
 }
 
+# Computing credible intervals of the different parameters
+cred_intervals <- function(sum_stat_mat){
+  n <- nrow(sum_stat_mat)
+  cred.int <- matrix(NA, nrow=n, ncol=3)
+  for (i in 1:n){
+    cred.int[i, ] <- quantile(sum_stat_mat[i,], c(0.05,0.5,0.95))
+    if (cred.int[i,1] < 0){cred.int[i,1] <- 0.0}
+  }
+  return(cred.int)
+}
+
+
+# FDP and Power for Current Methods
 compute_empirical_FDP_LBOND <- function(mixture.sim, alpha=0.1){
-  pvals <- convert_Z_to_Pval(mixture.sim)
-  return(alpha_inv(pvals, alpha))
+  lbond_results <- LBOND(mixture.sim$Pval, alpha)
+  lbond_power <- sum(((lbond_results$discoveries == 1) & (mixture.sim$true_signals == 1))) / sum((mixture.sim$true_signals == 1))
+  lbond_fdp <- sum((lbond_results$discoveries == 1) & (mixture.sim$true_signals == 0)) / sum((mixture.sim$true_signals == 0))
+  lbond_results$power <- lbond_power
+  lbond_results$fdp <- lbond_fdp
+  return(lbond_results)
 }
 
 compute_empirical_FDP_LBORD <- function(mixture.sim, alpha=0.1){
-  pvals <- convert_Z_to_Pval(mixture.sim)  
-  return(LBOND(pvals, alpha))
+  lbord_results <- LBORD(mixture.sim$Pval, alpha)
+  lbord_power <- sum(((lbord_results$discoveries == 1) & (mixture.sim$true_signals == 1))) / sum((mixture.sim$true_signals == 1))
+  lbord_fdp <- sum((lbord_results$discoveries == 1) & (mixture.sim$true_signals == 0)) / sum((mixture.sim$true_signals == 0))
+  lbord_results$power <- lbord_power
+  lbord_results$fdp <- lbord_fdp
+  return(lbord_results)
 }
 
-compute_empirical_FDP_Ainvesting <- function(mixture.sim){
-  
+compute_empirical_FDP_Ainvesting <- function(mixture.sim, alpha=0.1){
+  alpha_inv_results <- alpha_inv(mixture.sim$Pval, alpha)
+  alpha_inv_power <- sum(((alpha_inv_results$discoveries == 1) & (mixture.sim$true_signals == 1))) / sum((mixture.sim$true_signals == 1))
+  alpha_inv_fdp <- sum((alpha_inv_results$discoveries == 1) & (mixture.sim$true_signals == 0)) / sum((mixture.sim$true_signals == 0))
+  alpha_inv_results$power <- alpha_inv_power
+  alpha_inv_results$fdp <- alpha_inv_fdp
+  return(alpha_inv_results)
 }
 
-
-bayesian_Power <- function(pi0=0.9, mu=2, sigma2=1, alpha=0.1){
-  z.hat <- get_roots(BayesFDR, alpha, mus=mu, pi0s=pi0, sigma2s=sigma2)
-  prob_signals <- pi0 *(1-pnorm(z.hat)) + (1-pi0)*(1-pnorm((z.hat-mu)/sqrt(sigma2)))
-  fdr <- BayesFDR(z.hat, mu, alpha=alpha, pi0 = pi0, sigma2 = 1) + alpha
-  power <- (prob_signals * (1-fdr)) / (1 - pi0)
-  print(sprintf("FDR : %f , Power : %f, Z.hat : %f", fdr, power, z.hat))
-  # return(power)
+# Getting the FDP and Power for our method
+bayesian_criteria <- function(pi0s, mus, sigma2s, alpha=0.1){
+  n <- length(pi0s)
+  if (length(pi0s) != length(mus)){stop("Not good!")}
+  z.hat <- rep(NA, n)
+  for (i in 1:n){
+    z.hat[i] <- get_roots(BayesFDR, alpha, mus=mu[i], pi0s=pi0s[i], sigma2s=sigma2s[i])
+  }
+  return(z.hat)
 }
